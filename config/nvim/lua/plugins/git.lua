3ad567aa3c1411031b08d7e06d2bda772c7a0ee0
Rodrigo Eliseu <rodelrod@hotmail.com>
2026-03-10 00:37:04 +0100

    neovim: add neogit + diffview

---
+++ config/nvim/lua/plugins/git.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          require("neogit").open()
+        end,
+        desc = "Neogit",
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      integrations = {
+        diffview = true,
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewToggleFiles",
+    },
+  },
+}
