d7dc8497487133f33bb45f6dc813ba21cea2a289
Rodrigo Eliseu <rodelrod@hotmail.com>
2026-06-05 23:22:26 +0200

    fix: alias for z as cd was inverted

--- home/shared.nix
+++ home/shared.nix
@@ -86,7 +86,7 @@ in
       ll = "eza -lah --git --icons=auto --group-directories-first";
       la = "eza -a --group-directories-first";
       lt = "eza --tree --level=2 --icons=auto";
-      z = "cd";
+      cd = "z";
       gs = "git status";
       doom = "${doomCorePath}/bin/doom";
       new-nix-project = "${config.home.homeDirectory}/dotfiles/scripts/new-nix-project.sh";
